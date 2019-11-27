d539d2dc0e641617c5122a7df4f3f565d71bb298
Jimmy <jimmyskripchuk@gmail.com>
2019-11-27 13:00:27 -0500

    abstracted training

--- examples/training.py
+++ examples/training.py
@@ -2,6 +2,64 @@
 import pommerman
 from pommerman import agents
 
+from collections import defaultdict
+import queue
+import random
+
+import numpy as np
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+gamma = 0.99
+
+class QNet(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(QNet, self).__init__()
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+
+        self.fc1 = nn.Linear(num_inputs, 128)
+        self.fc2 = nn.Linear(128, num_outputs)
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform(m.weight)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        qvalue = self.fc2(x)
+        return qvalue
+
+    @classmethod
+    def train_model(cls, online_net, target_net, optimizer, batch):
+        states = torch.stack(batch.state)
+        next_states = torch.stack(batch.next_state)
+        actions = torch.Tensor(batch.action).float()
+        rewards = torch.Tensor(batch.reward)
+        masks = torch.Tensor(batch.mask)
+
+        pred = online_net(states).squeeze(1)
+        next_pred = target_net(next_states).squeeze(1)
+
+        pred = torch.sum(pred.mul(actions), dim=1)
+
+        target = rewards + masks * gamma * next_pred.max(1)[0]
+
+        loss = F.mse_loss(pred, target.detach())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        return loss
+
+    def get_action(self, input):
+        qvalue = self.forward(input)
+        #print(qvalue)
+        _, action = torch.max(qvalue, 1)
+        return action.numpy()[0]
 
 def main():
     '''Simple function to bootstrap a game.
@@ -26,13 +84,17 @@ def main():
     # Make the "Free-For-All" environment using the agent list
     env = pommerman.make('PommeFFACompetition-v0', agent_list)
     """
+    INPUT_SIZE = 11*11
+    OUTPUT_SIZE = 6
 
+    TRAINING_NET = QNet(INPUT_SIZE,OUTPUT_SIZE)
     NUM_EPOCHS = 3
     NUM_ROLLOUTS = 2
+    EPSILON = 0.2
 
     for epoch in range(NUM_EPOCHS):
         agent_list = [
-                agents.JukeBotDeep(),
+                agents.JukeBotDeep(train_net = TRAINING_NET, epsilon=EPSILON, debug=True),
                 agents.SimpleAgent(),
                 agents.SimpleAgent(),
                 agents.SimpleAgent()
@@ -53,22 +115,6 @@ def main():
             env.close()
 
         print("DONE EPOCH:",epoch)
-
-
-
-"""
-    n=1
-    for i_episode in range(n):
-        state = env.reset()
-        done = False
-        while not done:
-            env.render()
-            actions = env.act(state)
-            state, reward, done, info = env.step(actions)
-        print('Episode {} finished'.format(i_episode))
-    
-    env.close()
-    """
 
 
 if __name__ == '__main__':
--- pommerman/agents/juke_bot_deep.py
+++ pommerman/agents/juke_bot_deep.py
@@ -12,76 +12,40 @@ from .. import constants
 from .. import utility
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-gamma = 0.99
-
-class QNet(nn.Module):
-    def __init__(self, num_inputs, num_outputs):
-        super(QNet, self).__init__()
-        self.num_inputs = num_inputs
-        self.num_outputs = num_outputs
-
-        self.fc1 = nn.Linear(num_inputs, 128)
-        self.fc2 = nn.Linear(128, num_outputs)
-
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_uniform(m.weight)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        qvalue = self.fc2(x)
-        return qvalue
-
-    @classmethod
-    def train_model(cls, online_net, target_net, optimizer, batch):
-        states = torch.stack(batch.state)
-        next_states = torch.stack(batch.next_state)
-        actions = torch.Tensor(batch.action).float()
-        rewards = torch.Tensor(batch.reward)
-        masks = torch.Tensor(batch.mask)
-
-        pred = online_net(states).squeeze(1)
-        next_pred = target_net(next_states).squeeze(1)
-
-        pred = torch.sum(pred.mul(actions), dim=1)
-
-        target = rewards + masks * gamma * next_pred.max(1)[0]
-
-        loss = F.mse_loss(pred, target.detach())
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        return loss
-
-    def get_action(self, input):
-        qvalue = self.forward(input)
-        #print(qvalue)
-        _, action = torch.max(qvalue, 1)
-        return action.numpy()[0]
-
-
 class JukeBotDeep(BaseAgent):
     """This is a baseline agent. After you can beat it, submit your agent to
     compete.
     """
 
+
     def __init__(self, *args, **kwargs):
+        #new_args = args
+        net = kwargs["train_net"]
+        epsilon = kwargs["epsilon"]
+        debug = kwargs["debug"]
+        del kwargs["train_net"]
+        del kwargs["epsilon"]
+        del kwargs["debug"]
+
         super(JukeBotDeep, self).__init__(*args, **kwargs)
 
-        self.target_net = QNet(11*11, 6)
+
+        
+        #self.target_net = QNet(11*11, 6)
+        self.target_net = net
+        self.epsilon = epsilon
+
+        if debug:
+            print(self.epsilon)
 
     def act(self, obs, action_space):
         state = obs['board']
         flat = state.flatten()
         #print(flat)
-        eps = 0
+
         flat = torch.tensor([flat]).float()
         
-        return self.get_action(flat,self.target_net,eps,action_space)
+        return self.get_action(flat,self.target_net,self.epsilon,action_space)
 
     def get_action(self, state, target_net, epsilon, action_space):
         if np.random.rand() <= epsilon:
